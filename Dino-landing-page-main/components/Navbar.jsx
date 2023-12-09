608036149d2a2c685645ece37369e2a0b8f61c2e
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-12-10 01:50:04 +0530

    Main landing page added

--- Dino-landing-page-main/components/Navbar.jsx
+++ Dino-landing-page-main/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import Logo from '@/assets/dragon.png';
-import Image from 'next/image';
-import ColoredHamBurger from '@/assets/burger-menu-colored.svg';
-import WhiteHamBurger from '@/assets/burger-menu-white.svg';
+import React, { useState } from "react";
+import Logo from "@/assets/güvenli.png";
+
+import Image from "next/image";
+import ColoredHamBurger from "@/assets/burger-menu-colored.svg";
+import WhiteHamBurger from "@/assets/burger-menu-white.svg";
 
 const Navbar = ({ hasScrolled }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,8 +14,8 @@ const Navbar = ({ hasScrolled }) => {
 
   const downloadWallet = () => {
     window.open(
-      'https://dinowallet.netlify.app/',
-      '_blank' // <- This is what makes it open in a new window.
+      "http://localhost:3000",
+      "_blank" // <- This is what makes it open in a new window.
     );
   };
 
@@ -25,11 +26,11 @@ const Navbar = ({ hasScrolled }) => {
           <Image
             src={Logo}
             alt="logo"
-            width={90}
+            width={80}
             className="w-[80px] bg-transparent"
           />
           <div className="hidden small-lg:flex pl-[20px] text-[#222]  text-[0.875rem] leading-[1.3]">
-            <span className="text-[2rem] font-bold leading-10">Dino</span>
+            <span className="text-[2rem] font-bold leading-10">Güvenli</span>
           </div>
         </div>
         <div className="hidden md:flex gap-[10px] ">
@@ -38,15 +39,17 @@ const Navbar = ({ hasScrolled }) => {
           </button>
           <button
             onClick={downloadWallet}
-            className="bg-primary text-white mr-[5px] hover:bg-secondary hover:text-secondary-text pt-[9px] pb-[8px] px-5  text-[0.875rem] leading-[110%] font-medium rounded-[12px]">
+            className="bg-primary text-white mr-[5px] hover:bg-secondary hover:text-secondary-text pt-[9px] pb-[8px] px-5  text-[0.875rem] leading-[110%] font-medium rounded-[12px]"
+          >
             Download Wallet
           </button>
         </div>
         <div
           onClick={toggleMenu}
           className={`h-[62px] cursor-pointer w-[62px] flex items-center justify-center overflow-hidden small-lg:hidden rounded-[10px] ${
-            isMenuOpen && 'bg-primary'
-          }`}>
+            isMenuOpen && "bg-primary"
+          }`}
+        >
           <button className="">
             <Image
               src={isMenuOpen ? WhiteHamBurger : ColoredHamBurger}
@@ -60,8 +63,9 @@ const Navbar = ({ hasScrolled }) => {
 
       <div
         className={`bg-white fixed top-[78.8px] translate-y-0 small-lg:hidden  ${
-          !isMenuOpen && 'translate-y-[-135%]'
-        }  duration-500 transition-all ease-normal w-full  py-4 h-[calc(100vh-5rem)] `}>
+          !isMenuOpen && "translate-y-[-135%]"
+        }  duration-500 transition-all ease-normal w-full  py-4 h-[calc(100vh-5rem)] `}
+      >
         <div className="w-[90%] mx-auto">
           <p className="block py-2 px-4 text-primary-text">Product</p>
           <p className="block py-2 px-4 text-primary-text">About</p>
@@ -80,8 +84,9 @@ const Navbar = ({ hasScrolled }) => {
 
       <div
         className={`absolute transition-all duration-200 ${
-          (hasScrolled && 'opacity-[100%]') || (isMenuOpen && 'opacity-[100%]')
-        } ease-in-out top-0 opacity-0 group-hover:opacity-[100%] w-full h-[78.8px] bg-white`}></div>
+          (hasScrolled && "opacity-[100%]") || (isMenuOpen && "opacity-[100%]")
+        } ease-in-out top-0 opacity-0 group-hover:opacity-[100%] w-full h-[78.8px] bg-white`}
+      ></div>
     </div>
   );
 };
